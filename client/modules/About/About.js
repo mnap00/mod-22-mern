9411c58e1be3823d58ed47f54658a62d1e053e76
mnap <learn@10g.pl>
2018-06-10 18:44:22 +0200

    Add about module

---
+++ client/modules/About/About.js
@@ -0,0 +1,33 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+//import styles from './About.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About this Blog</h2>
+        <p>This is test MERN blog</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+About.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);
